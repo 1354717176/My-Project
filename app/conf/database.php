95709e9fd091f47b14b983ace69a91b422a68a7e
yanghuan <1354717176@qq.com>
2018-07-22 11:50:31 +0800

    增加 刷新浏览器时保存当前标签状态功能 增加 气泡提示&弹出框、模态框、选项卡&折叠面板、图片、徽章&标签、轮播组件页面

--- app/conf/database.php
+++ app/conf/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'localhost',
+    'hostname'        => '192.168.1.105',
     // 数据库名
-    'database'        => 'thinkphp5',
+    'database'        => 'thinkphp',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
